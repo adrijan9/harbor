0de92991764c95481dfc9075e82166ff4a4c9881
Adrijan Mitrov <adrijan9@gmail.com>
2026-02-22 15:35:18 +0100

    Introduce PHP-CS-Fixer configuration

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@auto' => true,
+        '@auto:risky' => true,
+        '@PhpCsFixer' => true,
+        'not_operator_with_successor_space' => true,
+        'php_unit_internal_class' => false,
+        'php_unit_method_casing' => ['case' => 'snake_case'],
+        'php_unit_test_class_requires_covers' => false,
+    ])
+    // 💡 by default, Fixer looks for `*.php` files excluding `./vendor/` - here, you can groom this config
+    ->setFinder(
+        (new Finder())
+            // 💡 root folder to check
+            ->in(__DIR__)
+            // 💡 additional files, eg bin entry file
+            // ->append([__DIR__.'/bin-entry-file'])
+            // 💡 folders to exclude, if any
+            // ->exclude([/* ... */])
+            // 💡 path patterns to exclude, if any
+            // ->notPath([/* ... */])
+            // 💡 extra configs
+            // ->ignoreDotFiles(false) // true by default in v3, false in v4 or future mode
+            // ->ignoreVCS(true) // true by default
+    )
+;
