b924293c335de69f60439da3829c1ec3b85be4cb
Adrijan Mitrov <adrijan9@gmail.com>
2026-02-22 22:19:23 +0100

    add dock block

--- documentation/index.php
+++ documentation/index.php
@@ -6,6 +6,11 @@ require __DIR__.'/../vendor/autoload.php';
 
 use Harbor\Router\Router;
 
+/*
+ * This file contains global configuration environment variables for the site.
+ * You can access these variables in your route entries using the `config()` function.
+ * For example, `config('app_name')` will return "Harbor Site".
+ */
 new Router(
     __DIR__.'/routes.php',
     __DIR__.'/config.php',
